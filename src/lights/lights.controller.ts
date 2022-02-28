e80dd5980fad4dff64d745accbfd2ef8dbaef0eb
Gabriele Partiti <gabrielepartiti@icloud.com>
2022-02-28 12:57:34 +0100

    fix date sunrise sunset

--- src/lights/lights.controller.ts
+++ src/lights/lights.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Logger } from '@nestjs/common';
 import { LightsService } from './lights.service';
 import { Cron, CronExpression } from '@nestjs/schedule';
+import { SunriseSunsetDate } from './bulb/interfaces';
 
 @Controller('lights')
 export class LightsController {
@@ -11,7 +12,7 @@ export class LightsController {
   @Cron(CronExpression.EVERY_DAY_AT_MIDNIGHT)
   async getSunriseSunset() {
     this.logger.log('Get Sunrise and Sunset for Today');
-    await this.service.saveSunriseSunSet();
+    await this.service.saveSunriseSunSet(SunriseSunsetDate.TODAY);
   }
 
   @Cron(CronExpression.EVERY_SECOND)
