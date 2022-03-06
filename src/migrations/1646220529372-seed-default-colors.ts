30898a40583dca9dc93bc5f082cc5413245f6bae
Gabriele Partiti <gabrielepartiti@icloud.com>
2022-03-06 18:40:25 +0100

    add seed for device meross cloud

--- src/migrations/1646220529372-seed-default-colors.ts
+++ src/migrations/1646220529372-seed-default-colors.ts
@@ -5,7 +5,7 @@ export class seedDefaultColors1646220529372 implements MigrationInterface {
 
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.query(
-      `INSERT INTO \`casita\`.\`colors\` (\`id\`, \`scope\`, \`temperature\`, \`rgb\`, \`luminance\`, \`capacity\`) VALUES ('092cc0cc-7835-4c4d-85cf-cefe5a81e4ae', 'MORNING', 95, 16711825, 100, 6)`,
+      `INSERT INTO \`casita\`.\`colors\` (\`id\`, \`scope\`, \`temperature\`, \`rgb\`, \`luminance\`, \`capacity\`) VALUES ('092cc0cc-7835-4c4d-85cf-cefe5a81e4ae', 'MORNING', 1, 16755038, 100, 6)`,
     );
     await queryRunner.query(
       `INSERT INTO \`casita\`.\`colors\` (\`id\`, \`scope\`, \`temperature\`, \`rgb\`, \`luminance\`, \`capacity\`) VALUES ('7cf03e50-2800-4da6-a7c5-1bb4939ed41f', 'BEFORE_SUNSET_SUNRISE', 1, 16755038, 100, 6)`,
